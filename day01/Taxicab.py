da23cd79355f4caf0b4af7fa95c6da67e31d6bdf
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-05 20:08:58 +0100

    Removing get_direction and incorporating the direction into get_distance

--- day01/Taxicab.py
+++ day01/Taxicab.py
@@ -1,13 +1,20 @@
 class Taxicab:
 
-    def get_distance(self, directions):
+    def get_distance(self, instructions):
         distance = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
+        directions = {'NL': 'W',
+                      'SL': 'E',
+                      'EL': 'N',
+                      'WL': 'S',
+                      'NR': 'E',
+                      'SR': 'W',
+                      'ER': 'S',
+                      'WR': 'N'}
         towards = "N"
         visited = [[0, 0]]
-        
-        instructions = self.get_parsed_instructions(directions)
-        for instruction in instructions:
-            towards = self.get_direction(towards, instruction[0])
+        parsed_instructions = self.parse_instructions(instructions)
+        for instruction in parsed_instructions:
+            towards = directions[towards + instruction[0]]
             for x in range(distance[towards], distance[towards] + instruction[1]):
                 distance[towards] += 1
                 x = distance['N'] - distance['S']
@@ -17,19 +24,9 @@ class Taxicab:
                 else:
                     visited.append([x, y])
 
-    def get_parsed_instructions(self, instructions):
+    def parse_instructions(self, instructions):
         parsed_instructions = list()
         for instruction in instructions.split(","):
             parsed_instructions.append([instruction.strip()[0], int(instruction.strip()[1:])])
         return parsed_instructions
 
-    def get_direction(self, current_towards, direction):
-        options = {'NL': 'W',
-                   'SL': 'E',
-                   'EL': 'N',
-                   'WL': 'S',
-                   'NR': 'E',
-                   'SR': 'W',
-                   'ER': 'S',
-                   'WR': 'N'}
-        return options[current_towards + direction]
--- day01/TaxicabTest.py
+++ day01/TaxicabTest.py
@@ -10,22 +10,11 @@ class TaxicabTest(unittest.TestCase):
         self.assertEquals(taxicab.get_distance("R10, R2, R2, R8"), 8)
         self.assertEquals(taxicab.get_distance("R10, R2, R2, R1, R2"), 11)
 
-    def test_get_instructions_parsed(self):
+    def test_parse_instructions(self):
         taxicab = Taxicab()
-        self.assertEquals(taxicab.get_parsed_instructions("R2, L3"), [["R", 2], ["L", 3]])
-        self.assertEquals(taxicab.get_parsed_instructions("R2, R2, R2"), [["R", 2], ["R", 2], ["R", 2]])
-        self.assertEquals(taxicab.get_parsed_instructions("R5, L5, R5, R3"), [["R", 5], ["L", 5], ["R", 5], ["R", 3]])
-
-    def test_get_direction(self):
-        taxicab = Taxicab()
-        self.assertEquals(taxicab.get_direction("N", "R"), "E")
-        self.assertEquals(taxicab.get_direction("N", "L"), "W")
-        self.assertEquals(taxicab.get_direction("W", "R"), "N")
-        self.assertEquals(taxicab.get_direction("W", "L"), "S")
-        self.assertEquals(taxicab.get_direction("E", "R"), "S")
-        self.assertEquals(taxicab.get_direction("E", "L"), "N")
-        self.assertEquals(taxicab.get_direction("S", "R"), "W")
-        self.assertEquals(taxicab.get_direction("S", "L"), "E")
+        self.assertEquals(taxicab.parse_instructions("R2, L3"), [["R", 2], ["L", 3]])
+        self.assertEquals(taxicab.parse_instructions("R2, R2, R2"), [["R", 2], ["R", 2], ["R", 2]])
+        self.assertEquals(taxicab.parse_instructions("R5, L5, R5, R3"), [["R", 5], ["L", 5], ["R", 5], ["R", 3]])
 
 if __name__ == '__main__':
     unittest.main()
