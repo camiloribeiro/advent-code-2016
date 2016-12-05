2d80caf662f5a5f3a7134bee05ca848270d34a87
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-05 17:54:27 +0100

    adding get parsed instructions with coordenates

--- day01/Taxicab.py
+++ day01/Taxicab.py
@@ -3,5 +3,11 @@ class Taxicab:
     def get_distance(self, directions):
         return directions
 
+    def get_parsed_instructions(self, instructions):
+        parsed_instructions = list()
+        for instruction in instructions.split(","):
+            parsed_instructions.append(self.parse_instruction(instruction))
+        return parsed_instructions
+
     def parse_instruction(self, instruction):
         return [instruction.strip()[0], int(instruction.strip()[1:])]
--- day01/TaxicabTest.py
+++ day01/TaxicabTest.py
@@ -7,6 +7,12 @@ class TaxicabTest(unittest.TestCase):
         taxicab = Taxicab()
         self.assertEquals(taxicab.get_distance("R2, L3"), "R2, L3")
 
+    def test_get_instructions_parsed(self):
+        taxicab = Taxicab()
+        self.assertEquals(taxicab.get_parsed_instructions("R2, L3"), [["R", 2], ["L", 3]])
+        self.assertEquals(taxicab.get_parsed_instructions("R2, R2, R2"), [["R", 2], ["R", 2], ["R", 2]])
+        self.assertEquals(taxicab.get_parsed_instructions("R5, L5, R5, R3"), [["R", 5], ["L", 5], ["R", 5], ["R", 3]])
+
     def test_parse_instruction(self):
         taxicab = Taxicab()
         self.assertEquals(taxicab.parse_instruction("R2"), ["R", 2])
