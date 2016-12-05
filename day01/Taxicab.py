d7391f301031bf0b2e1739c9fca81e0a2cb9741c
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-05 17:42:01 +0100

    Adding parser for single instruction

--- day01/Taxicab.py
+++ day01/Taxicab.py
@@ -2,3 +2,6 @@ class Taxicab:
 
     def get_distance(self, directions):
         return directions
+
+    def parse_instruction(self, instruction):
+        return [instruction[0], int(instruction[1:])]
--- day01/TaxicabTest.py
+++ day01/TaxicabTest.py
@@ -7,5 +7,13 @@ class TaxicabTest(unittest.TestCase):
         taxicab = Taxicab()
         self.assertEquals(taxicab.get_distance("R2, L3"), "R2, L3")
 
+    def test_parse_instruction(self):
+        taxicab = Taxicab()
+        self.assertEquals(taxicab.parse_instruction("R2"), ["R", 2])
+        self.assertEquals(taxicab.parse_instruction("R3"), ["R", 3])
+        self.assertEquals(taxicab.parse_instruction("R5"), ["R", 5])
+        self.assertEquals(taxicab.parse_instruction("L3"), ["L", 3])
+        self.assertEquals(taxicab.parse_instruction("N22"), ["N", 22])
+
 if __name__ == '__main__':
     unittest.main()
