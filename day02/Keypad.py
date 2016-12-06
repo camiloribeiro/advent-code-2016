d148504aec7da319a5484f3c45db7b2f5e92b321
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-06 02:52:10 +0100

    Adding get_position method to find next key based on current

--- day02/Keypad.py
+++ day02/Keypad.py
@@ -2,3 +2,18 @@ class Keypad:
 
     def read_instruction(self, instruction):
         return instruction
+
+    def get_position(self, current, instruction):
+        directions = {'5U': '2', '5D': '8','5L': '4', '5R': '6',
+                      '2D': '5', '2L': '1','2R': '3',
+                      '4U': '1', '4D': '7','4R': '5',
+                      '6U': '3', '6D': '9','6L': '5',
+                      '8U': '5', '8L': '7','8R': '9',
+                      '1D': '4', '1R': '2',
+                      '3D': '6', '3L': '2',
+                      '7U': '4', '7R': '8',
+                      '9U': '6', '9L': '8'}
+        try:
+            return directions[current + instruction]
+        except KeyError:
+            return current
--- day02/KeypadTest.py
+++ day02/KeypadTest.py
@@ -8,5 +8,53 @@ class KeypadTest(unittest.TestCase):
         keypad = Keypad()
         self.assertEquals(keypad.read_instruction("U"), "U")
 
+    def test_get_position(self):
+        keypad = Keypad()
+
+        self.assertEquals(keypad.get_position("1", "U"), "1")
+        self.assertEquals(keypad.get_position("1", "D"), "4")
+        self.assertEquals(keypad.get_position("1", "L"), "1")
+        self.assertEquals(keypad.get_position("1", "R"), "2")
+        
+        self.assertEquals(keypad.get_position("2", "U"), "2")
+        self.assertEquals(keypad.get_position("2", "D"), "5")
+        self.assertEquals(keypad.get_position("2", "L"), "1")
+        self.assertEquals(keypad.get_position("2", "R"), "3")
+
+        self.assertEquals(keypad.get_position("3", "U"), "3")
+        self.assertEquals(keypad.get_position("3", "D"), "6")
+        self.assertEquals(keypad.get_position("3", "L"), "2")
+        self.assertEquals(keypad.get_position("3", "R"), "3")
+
+        self.assertEquals(keypad.get_position("4", "U"), "1")
+        self.assertEquals(keypad.get_position("4", "D"), "7")
+        self.assertEquals(keypad.get_position("4", "L"), "4")
+        self.assertEquals(keypad.get_position("4", "R"), "5")
+
+        self.assertEquals(keypad.get_position("5", "U"), "2")
+        self.assertEquals(keypad.get_position("5", "D"), "8")
+        self.assertEquals(keypad.get_position("5", "L"), "4")
+        self.assertEquals(keypad.get_position("5", "R"), "6")
+
+        self.assertEquals(keypad.get_position("6", "U"), "3")
+        self.assertEquals(keypad.get_position("6", "D"), "9")
+        self.assertEquals(keypad.get_position("6", "L"), "5")
+        self.assertEquals(keypad.get_position("6", "R"), "6")
+
+        self.assertEquals(keypad.get_position("7", "U"), "4")
+        self.assertEquals(keypad.get_position("7", "D"), "7")
+        self.assertEquals(keypad.get_position("7", "L"), "7")
+        self.assertEquals(keypad.get_position("7", "R"), "8")
+
+        self.assertEquals(keypad.get_position("8", "U"), "5")
+        self.assertEquals(keypad.get_position("8", "D"), "8")
+        self.assertEquals(keypad.get_position("8", "L"), "7")
+        self.assertEquals(keypad.get_position("8", "R"), "9")
+
+        self.assertEquals(keypad.get_position("9", "U"), "6")
+        self.assertEquals(keypad.get_position("9", "D"), "9")
+        self.assertEquals(keypad.get_position("9", "L"), "8")
+        self.assertEquals(keypad.get_position("9", "R"), "9")
+
 if __name__ == '__main__':
     unittest.main()
