8ab9a0e6f191e8472eee6849fdf1e3cf17ad8894
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-07 00:50:47 +0100

    getting all valid triangles

--- day03/ThreeSides.py
+++ day03/ThreeSides.py
@@ -1,4 +1,11 @@
 class ThreeSides:
 
-    def is_a_valid_triangle(self, side_a, side_b, side_c):
-        return ((side_a + side_b > side_c) and (side_b + side_c > side_a) and (side_c + side_a > side_b))
+    def is_a_valid_triangle(self, ts):
+        return ((ts[0] + ts[1] > ts[2]) and (ts[1] + ts[2] > ts[0]) and (ts[2] + ts[0] > ts[1]))
+
+    def get_valid_triangles(self, data):
+        triangles = 0
+        for candidate in data.strip().split("\n"):
+            if self.is_a_valid_triangle(map(int, candidate.split())):
+                triangles += 1
+        return triangles
--- day03/ThreeSidesTest.py
+++ day03/ThreeSidesTest.py
@@ -7,22 +7,41 @@ class ThreeSidesTest(unittest.TestCase):
     def test_is_triangle(self):
         # My blog post from 2010 http://www.bugbang.com.br/uma-introducao-a-tdd-com-junit/
         three_sides = ThreeSides()
-        self.assertEquals(three_sides.is_a_valid_triangle(3, 4, 5), True)
-        self.assertEquals(three_sides.is_a_valid_triangle(5, 10, 25), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(2, 9, 10), True)
-        self.assertEquals(three_sides.is_a_valid_triangle(20, 20, 20), True)
-        self.assertEquals(three_sides.is_a_valid_triangle(20, 20, 30), True)
-        self.assertEquals(three_sides.is_a_valid_triangle(20, 30, 20), True)
-        self.assertEquals(three_sides.is_a_valid_triangle(30, 20, 20), True)
-        self.assertEquals(three_sides.is_a_valid_triangle(0, 2, 9), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(3, -2, 9), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(5, 6, 11), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(5, 11, 6), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(11, 6, 5), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(5, 6, 12), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(0, 0, 0), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(5, 12, 6), False)
-        self.assertEquals(three_sides.is_a_valid_triangle(12, 5, 6), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([3, 4, 5]), True)
+        self.assertEquals(three_sides.is_a_valid_triangle([5, 10, 25]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([2, 9, 10]), True)
+        self.assertEquals(three_sides.is_a_valid_triangle([20, 20, 20]), True)
+        self.assertEquals(three_sides.is_a_valid_triangle([20, 20, 30]), True)
+        self.assertEquals(three_sides.is_a_valid_triangle([20, 30, 20]), True)
+        self.assertEquals(three_sides.is_a_valid_triangle([30, 20, 20]), True)
+        self.assertEquals(three_sides.is_a_valid_triangle([0, 2, 9]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([3, -2, 9]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([5, 6, 11]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([5, 11, 6]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([11, 6, 5]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([5, 6, 12]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([0, 0, 0]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([5, 12, 6]), False)
+        self.assertEquals(three_sides.is_a_valid_triangle([12, 5, 6]), False)
+
+    def test_get_number_of_valid_triangles(self):
+        three_sides = ThreeSides()
+        self.assertEquals(three_sides.get_valid_triangles("   3     4    5\n" +
+                                                          "   5    10    25\n" +
+                                                          "   2     9    10\n" +
+                                                          "  20    20    20\n" +
+                                                          "  20    20    30\n" +
+                                                          "  20    30    20\n" +
+                                                          "  30    20    20\n" +
+                                                          "   0     2    9\n" +
+                                                          "   3    -2    9\n" +
+                                                          "   5     6    11\n" +
+                                                          "   5    11    6\n" +
+                                                          "  11    6    5\n" +
+                                                          "   5     6    12\n" +
+                                                          "   0     0    0\n" +
+                                                          "   5    12    6\n" +
+                                                          "  12    5    6"), 6)
 
 if __name__ == '__main__':
     unittest.main()
