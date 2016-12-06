b4da59ce887586d14d59c6fe9e68eb171eb764a9
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-07 00:04:49 +0100

    Adding implementation... and tests

--- day03/ThreeSides.py
+++ day03/ThreeSides.py
@@ -1,4 +1,4 @@
 class ThreeSides:
 
     def is_a_valid_triangle(self, side_a, side_b, side_c):
-        return True
+        return ((side_a + side_b > side_c) and (side_b + side_c > side_a) and (side_c + side_a > side_b))
--- day03/ThreeSidesTest.py
+++ day03/ThreeSidesTest.py
@@ -5,8 +5,24 @@ from ThreeSides import ThreeSides
 class ThreeSidesTest(unittest.TestCase):
 
     def test_is_triangle(self):
+        # My blog post from 2010 http://www.bugbang.com.br/uma-introducao-a-tdd-com-junit/
         three_sides = ThreeSides()
         self.assertEquals(three_sides.is_a_valid_triangle(3, 4, 5), True)
+        self.assertEquals(three_sides.is_a_valid_triangle(5, 10, 25), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(2, 9, 10), True)
+        self.assertEquals(three_sides.is_a_valid_triangle(20, 20, 20), True)
+        self.assertEquals(three_sides.is_a_valid_triangle(20, 20, 30), True)
+        self.assertEquals(three_sides.is_a_valid_triangle(20, 30, 20), True)
+        self.assertEquals(three_sides.is_a_valid_triangle(30, 20, 20), True)
+        self.assertEquals(three_sides.is_a_valid_triangle(0, 2, 9), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(3, -2, 9), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(5, 6, 11), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(5, 11, 6), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(11, 6, 5), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(5, 6, 12), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(0, 0, 0), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(5, 12, 6), False)
+        self.assertEquals(three_sides.is_a_valid_triangle(12, 5, 6), False)
 
 if __name__ == '__main__':
     unittest.main()
