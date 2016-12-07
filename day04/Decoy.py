3aa617eaa003221688cac47644f1b0cbfa606b13
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-08 00:53:27 +0100

    Returning sector id when room exists

--- day04/Decoy.py
+++ day04/Decoy.py
@@ -4,6 +4,9 @@ class Decoy:
         return room.replace(']', '').rsplit("[", 1)
 
     def check_checksum(self, room):
-        valid_elements = [x for x in list(room[0]) if x not in list("0123456789-")]
+        elements, sector_id = room[0].rsplit("-", 1)
+        valid_elements = [x for x in list(elements) if x not in list("0123456789-")]
         d = {i: valid_elements.count(i) for i in valid_elements}
-        return ''.join([v[0] for v in sorted(d.iteritems(), key=lambda(k, v): (-v, k))][:5]) == room[1]
+        if ''.join([v[0] for v in sorted(d.iteritems(), key=lambda(k, v): (-v, k))][:5]) == room[1]:
+            return int(sector_id)
+        return False
--- day04/DecoyTest.py
+++ day04/DecoyTest.py
@@ -10,9 +10,9 @@ class DecoyTest(unittest.TestCase):
 
     def test_check_checksum(self):
         decoy = Decoy()
-        self.assertEquals(decoy.check_checksum(["aaaaa-bbb-z-y-x-123", "abxyz"]), True)
-        self.assertEquals(decoy.check_checksum(["a-b-c-d-e-f-g-h-987", "abcde"]), True)
-        self.assertEquals(decoy.check_checksum(["not-a-real-room-404", "oarel"]), True)
+        self.assertEquals(decoy.check_checksum(["aaaaa-bbb-z-y-x-123", "abxyz"]), 123)
+        self.assertEquals(decoy.check_checksum(["a-b-c-d-e-f-g-h-987", "abcde"]), 987)
+        self.assertEquals(decoy.check_checksum(["not-a-real-room-404", "oarel"]), 404)
         self.assertEquals(decoy.check_checksum(["totally-real-room-200", "decoy"]), False)
 
 if __name__ == '__main__':
