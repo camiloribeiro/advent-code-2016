8b989d0f5d79c36e6f7ee9f7a6b0bec353b6f0e3
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-11 22:07:09 +0100

    Implementing firt part

--- day06/Noise.py
+++ day06/Noise.py
@@ -1,4 +1,12 @@
-class Noise:
+from collections import Counter
 
+
+class Noise:
     def get_most_repeated_char(self, data):
-        return "easter"
+        message, echos = "", [list(x) for x in data.split("\n")]
+        for x in range(0, len(echos[0])):
+            temp = []
+            for echo in echos:
+                temp.append(echo[x])
+            message += Counter(temp).most_common(1)[0][0]
+        return message
