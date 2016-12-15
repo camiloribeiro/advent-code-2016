34e9ab6a29bfc4108c0abaa69d1ec10a941fecfc
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-15 12:00:38 +0100

    Reducing code repetition

--- day06/NoiseTest.py
+++ day06/NoiseTest.py
@@ -4,42 +4,27 @@ from Noise import Noise
 
 class NoiseTest(unittest.TestCase):
 
+    data = "eedadn\n" + \
+            "drvtee\n" + \
+            "eandsr\n" + \
+            "raavrd\n" + \
+            "atevrs\n" + \
+            "tsrnev\n" + \
+            "sdttsa\n" + \
+            "rasrtv\n" + \
+            "nssdts\n" + \
+            "ntnada\n" + \
+            "svetve\n" + \
+            "tesnvt\n" + \
+            "vntsnd\n" + \
+            "vrdear\n" + \
+            "dvrsen\n" + \
+            "enarar"
+
     def test_get_message_with_most_repeated(self):
         noise = Noise()
-        data = "eedadn\n" + \
-               "drvtee\n" + \
-               "eandsr\n" + \
-               "raavrd\n" + \
-               "atevrs\n" + \
-               "tsrnev\n" + \
-               "sdttsa\n" + \
-               "rasrtv\n" + \
-               "nssdts\n" + \
-               "ntnada\n" + \
-               "svetve\n" + \
-               "tesnvt\n" + \
-               "vntsnd\n" + \
-               "vrdear\n" + \
-               "dvrsen\n" + \
-               "enarar"
-        self.assertEquals(noise.get_message(data, 0), "easter")
+        self.assertEquals(noise.get_message(self.data, 0), "easter")
 
     def test_get_message_with_least_repeated(self):
         noise = Noise()
-        data = "eedadn\n" + \
-               "drvtee\n" + \
-               "eandsr\n" + \
-               "raavrd\n" + \
-               "atevrs\n" + \
-               "tsrnev\n" + \
-               "sdttsa\n" + \
-               "rasrtv\n" + \
-               "nssdts\n" + \
-               "ntnada\n" + \
-               "svetve\n" + \
-               "tesnvt\n" + \
-               "vntsnd\n" + \
-               "vrdear\n" + \
-               "dvrsen\n" + \
-               "enarar"
-        self.assertEquals(noise.get_message(data, -1), "advent")
+        self.assertEquals(noise.get_message(self.data, -1), "advent")
