8cda27237e9cd1aa8f16b159031d2c3be977be20
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-17 16:38:07 +0100

    Adding skeleton

---
+++ day07/Ip.py
@@ -0,0 +1,5 @@
+class Ip:
+
+    def ParseIp(self, ip):
+        hypernet, ip_p1, ip_p2 = ip[4:8], ip[0:4], ip[8:10]
+        return True
---
+++ day07/IpTest.py
@@ -0,0 +1,8 @@
+import unittest
+from Ip import Ip
+
+
+class IpTest(unittest.TestCase):
+
+    def test_ip_parser(self):
+        True
