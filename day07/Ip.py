ed18120d880d1db93041fec0efd5e72d3e556a24
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-17 16:47:46 +0100

    adding check for abba

--- day07/Ip.py
+++ day07/Ip.py
@@ -2,3 +2,6 @@ class Ip:
 
     def parse(self, ip):
         return [ip[0:4], ip[5:9], ip[10:14]]
+
+    def check_abba(self, fragment):
+        return fragment[0] == fragment[-1] and fragment[1] == fragment[2]
--- day07/IpTest.py
+++ day07/IpTest.py
@@ -7,3 +7,11 @@ class IpTest(unittest.TestCase):
     def test_ip_parser(self):
         ip = Ip()
         self.assertEquals(ip.parse("abba[mnop]qrst"), ["abba", "mnop", "qrst"])
+
+    def test_abba(self):
+        ip = Ip()
+        self.assertEquals(ip.check_abba("abba"), True)
+        self.assertEquals(ip.check_abba("abbb"), False)
+        self.assertEquals(ip.check_abba("abaa"), False)
+        self.assertEquals(ip.check_abba("aaaa"), True)
+
