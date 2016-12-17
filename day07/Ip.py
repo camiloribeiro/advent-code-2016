7999d88e76a21102afe3649b869a1cca6e107666
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-17 16:54:16 +0100

    easy implementation, now it is time to fix the code

--- day07/Ip.py
+++ day07/Ip.py
@@ -5,3 +5,7 @@ class Ip:
 
     def check_abba(self, fragment):
         return fragment[0] == fragment[-1] and fragment[1] == fragment[2]
+
+    def support_tls(self, ip):
+        parsed_ip = self.parse(ip)
+        return (self.check_abba(parsed_ip[0]) or self.check_abba(parsed_ip[2]) and not self.check_abba(parsed_ip[1]))
--- day07/IpTest.py
+++ day07/IpTest.py
@@ -15,3 +15,9 @@ class IpTest(unittest.TestCase):
         self.assertEquals(ip.check_abba("abaa"), False)
         self.assertEquals(ip.check_abba("aaaa"), True)
 
+    def test_support_tls(self):
+        ip = Ip()
+        self.assertEquals(ip.support_tls("abba[mnop]qrst"), True)
+        self.assertEquals(ip.support_tls("abcd[bddb]xyyx"), False)
+        self.assertEquals(ip.support_tls("aaaa[qwer]tyui"), True)
+        #self.assertEquals(ip.support_tls("ioxxoj[asdfgh]zxcvbn"), True)
