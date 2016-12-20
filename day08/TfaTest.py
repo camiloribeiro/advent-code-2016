0b36756001971f3e9aad0ec75b5e46cadabec6ae
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-20 17:07:49 +0100

    Adding test for overflow on rotation

--- day08/TfaTest.py
+++ day08/TfaTest.py
@@ -22,3 +22,11 @@ class TfaTest(unittest.TestCase):
         self.assertItemsEqual(tfa.rotate_col(1, 1, display), [[1, 0, 1, 0, 0, 0, 0],
                                                               [1, 1, 1, 0, 0, 0, 0],
                                                               [0, 1, 0, 0, 0, 0, 0]])
+
+    def test_rotate_column_overflow(self):
+        tfa = Tfa()
+        display = [[0 for x in range(7)] for y in range(3)]
+        display = tfa.react(2, 3, display)
+        self.assertItemsEqual(tfa.rotate_col(1, 2, display), [[1, 1, 1, 0, 0, 0, 0],
+                                                              [1, 0, 1, 0, 0, 0, 0],
+                                                              [0, 1, 0, 0, 0, 0, 0]])
