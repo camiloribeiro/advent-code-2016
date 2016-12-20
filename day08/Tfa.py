133329edb0210b2f2797d661341880e83df0b85c
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-12-20 16:42:51 +0100

    basic refactoring

--- day08/Tfa.py
+++ day08/Tfa.py
@@ -1,12 +1,10 @@
 class Tfa:
     def lighted_elements(self):
-        w, h = 50, 8
-        display = [[0 for x in range(w)] for y in range(h)]
-        total = 0
+        display, lighted = [[0 for x in range(50)] for y in range(8)], 0
         for row in range(len(display)):
             for col in range(len(display[0])):
-                total += display[row][col]
-        return total
+                lighted += display[row][col]
+        return lighted
 
     def react(self, h, w, display):
         for row in range(0, h):
